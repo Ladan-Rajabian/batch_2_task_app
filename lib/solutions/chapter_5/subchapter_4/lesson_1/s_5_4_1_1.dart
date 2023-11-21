572c8762875b679ce6b09dac11e8fb6a706f5802
Ladan_Rajabian <ladanrajabian@gmail.com>
2023-11-21 16:26:05 +0100

    5.4.1.1 edited

--- lib/solutions/chapter_5/subchapter_4/lesson_1/s_5_4_1_1.dart
+++ lib/solutions/chapter_5/subchapter_4/lesson_1/s_5_4_1_1.dart
@@ -4,34 +4,13 @@ class S5411 extends StatelessWidget {
   const S5411({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MyButton();
-  }
-}
-
-class MyButton extends StatefulWidget {
-  const MyButton({super.key});
-
-  @override
-  State<MyButton> createState() => _MyButtonState();
-}
-
-class _MyButtonState extends State<MyButton> {
-  @override
-  Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: () {
         try {
-          throw Exception('fehler');
+          throw Exception('Error');
         } catch (error) {
-          final snackBar = SnackBar(
-              content: Text(error.toString()),
-              action: SnackBarAction(
-                label: 'error',
-                onPressed: () {
-                  // Some code to undo the change.
-                },
-              ));
-          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(error.toString())));
         }
       },
       child: const Text('Click me'),
